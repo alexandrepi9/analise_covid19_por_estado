32897dac2286f760a492a6c1bfce2a770730659e
Alexandre Tavares <pintoalexandre90@hotmail.com>
2025-01-18 18:20:20 -0300

    Consertando erro em nome de tabela

--- analise_covid_19.py
+++ analise_covid_19.py
@@ -5,14 +5,11 @@ import streamlit as st
 
 df = pd.read_csv("https://raw.githubusercontent.com/wcota/covid19br/refs/heads/master/cases-brazil-states.csv")
 
-df = df.rename(columns={'newDeaths': 'Novos óbitos', 'newCases': 'Novos casos', 'death_per_100k_inhabitants': 'Óbitos por 100 mil habitantes', 'totalCases_per_100k_inhabitants': 'Casos por 100 mil habitantes'})
+df = df.rename(columns={'newDeaths': 'Novos óbitos', 'newCases': 'Novos casos', 'deaths_per_100k_inhabitants': 'Óbitos por 100 mil habitantes', 'totalCases_per_100k_inhabitants': 'Casos por 100 mil habitantes'})
 
 estados = list(df['state'].unique())
 state = st.sidebar.selectbox('Escolha o estado', estados)
 
-#state = 'PE'
-
-#column = 'Casos por 100 mil habitantes'
 colunas = ['Novos óbitos', 'Novos casos', 'Óbitos por 100 mil habitantes', 'Casos por 100 mil habitantes']
 column = st.sidebar.selectbox('Selecione o tipo de informação', colunas)
 
